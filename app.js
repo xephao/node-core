c0bc32c4c8ca34ec68563af1adcc57318c756bd1
DESKTOP-HUUG7UQ\Nhan Nguyen <trongnhannt.it@gmail.com>
2018-06-14 00:22:18 +0700

    Commit First version

--- app.js
+++ app.js
@@ -12,3 +12,9 @@ app.listen(port, (err) => {
     }
     console.log('server is listening on ${port}')
 })
+
+var app = require('./app');
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+});
